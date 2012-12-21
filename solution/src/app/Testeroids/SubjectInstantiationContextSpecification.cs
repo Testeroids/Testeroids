af88692d660525a8fe985ef15ced44277c5bfa7b
Pedro Pombeiro <pedropombeiro@gmail.com>
2012-12-21 23:08:51 +0100

    Rename SubjectInstantiationTestContext to SubjectInstantiationContextSpecification. Make ContextSpecificationBase the base class for SubjectInstantiationContextSpecification and ContextSpecification.

--- solution/src/app/Testeroids/SubjectInstantiationContextSpecification.cs
+++ solution/src/app/Testeroids/SubjectInstantiationContextSpecification.cs
@@ -1,34 +1,20 @@
 ﻿// --------------------------------------------------------------------------------------------------------------------
-// <copyright file="SubjectInstantiationTestContext.cs" company="Testeroids">
+// <copyright file="SubjectInstantiationContextSpecification.cs" company="Testeroids">
 //   © 2012 Testeroids. All rights reserved.
 // </copyright>
 // --------------------------------------------------------------------------------------------------------------------
 
 namespace Testeroids
 {
-    using System.Diagnostics;
-
-    using NUnit.Framework;
+    using Testeroids.Aspects;
 
     /// <summary>
     ///   Implements the base class to define a Context/Specification style test fixture to test a class constructor.
     /// </summary>
     /// <typeparam name="TSubjectUnderTest"> Type of the class which constructor is to be tested. </typeparam>
-    public abstract class SubjectInstantiationTestContext<TSubjectUnderTest> : TestFixtureBase, IContextSpecification
+    public abstract class SubjectInstantiationContextSpecification<TSubjectUnderTest> : ContextSpecificationBase
         where TSubjectUnderTest : class
     {
-        #region Public Properties
-
-        /// <summary>
-        ///   Gets a value indicating whether there are prerequisite tests running. Here, no prerequisite tests are needed for constructor testing, but interface must be implemented.
-        /// </summary>
-        public bool ArePrerequisiteTestsRunning
-        {
-            get { return false; }
-        }
-
-        #endregion
-
         #region Properties
 
         /// <summary>
@@ -38,36 +24,12 @@ namespace Testeroids
 
         #endregion
 
-        #region Public Methods and Operators
-
-        /// <summary>
-        ///   The base set up.
-        /// </summary>
-        [SetUp]
-        public virtual void BaseSetUp()
-        {
-            this.PreTestSetUp();
-            this.EstablishContext();
-        }
-
-        /// <summary>
-        ///   Disposes the <see cref="Sut"/> and the context.
-        /// </summary>
-        public override void BaseTearDown()
-        {
-            this.DisposeContext();
-
-            base.BaseTearDown();
-        }
-
-        #endregion
-
         #region Methods
 
         /// <summary>
-        ///   This will be called by the ArrangeActAssertAspectAttribute aspect.
+        ///   This will be called by the <see cref="ArrangeActAssertAspectAttribute"/> aspect. Performs the "Act" part, or the logic which is to be tested (in this case, executing the constructor of the <typeparamref name="TSubjectUnderTest"/>.
         /// </summary>
-        protected void Because()
+        protected internal override void Because()
         {
             this.Sut = this.BecauseSutIsCreated();
         }
@@ -79,33 +41,9 @@ namespace Testeroids
         protected abstract TSubjectUnderTest BecauseSutIsCreated();
 
         /// <summary>
-        ///   Called to dispose all unmanaged resources used by the test.
+        ///   Performs additional initialization after the subject under test has been created. Not used in this scenario.
         /// </summary>
-        [DebuggerNonUserCode]
-        protected virtual void DisposeContext()
-        {
-        }
-
-        /// <summary>
-        ///   The arrange part.
-        /// </summary>
-        [DebuggerNonUserCode]
-        protected virtual void EstablishContext()
-        {
-        }
-
-        /// <summary>
-        /// This test is meant for internal library use only.
-        /// </summary>
-        [DebuggerNonUserCode]
-        protected virtual void PreTestSetUp()
-        {
-        }
-
-        /// <summary>
-        ///   This will be called by the ArrangeActAssertAspectAttribute aspect.
-        /// </summary>
-        protected void RunPrerequisiteTests()
+        protected override sealed void InitializeSubjectUnderTest()
         {
         }
 
