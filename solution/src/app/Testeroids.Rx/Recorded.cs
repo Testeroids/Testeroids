15c8600aa84fd1c702a9d8213bd9cc3592f25417
Pedro Pombeiro <pedropombeiro@gmail.com>
2013-02-28 19:06:54 +0100

    Rename RxHelper to "Recorded".
     - Add overloads for Recorded methods which take TimeSpan values.

--- solution/src/app/Testeroids.Rx/Recorded.cs
+++ solution/src/app/Testeroids.Rx/Recorded.cs
@@ -1,5 +1,5 @@
 ﻿// --------------------------------------------------------------------------------------------------------------------
-// <copyright file="RxHelper.cs" company="Testeroids">
+// <copyright file="Recorded.cs" company="Testeroids">
 //   © 2012-2013 Testeroids. All rights reserved.
 // </copyright>
 // --------------------------------------------------------------------------------------------------------------------
@@ -13,7 +13,7 @@ namespace Testeroids.Rx
     /// <summary>
     /// Defines Helper methods to use when testing Rx's <see cref="IObservable{T}"/> 
     /// </summary>
-    public static class RxHelper
+    public static class Recorded
     {
         #region Public Methods and Operators
 
@@ -28,6 +28,19 @@ namespace Testeroids.Rx
         public static Recorded<Notification<T>> OnCompleted<T>(long time)
         {
             return new Recorded<Notification<T>>(time, Notification.CreateOnCompleted<T>());
+        }
+
+        /// <summary>
+        /// Creates a <see cref="Recorded{Notification}"/> instance with <see cref="NotificationKind.OnCompleted"/> kind.
+        /// </summary>
+        /// <typeparam name="T">Type of the notifications.</typeparam>
+        /// <param name="time">Virtual time the value was produced on.</param>
+        /// <returns>
+        /// A new <see cref="Recorded{Notification}"/> instance with <see cref="NotificationKind.OnCompleted"/> kind.
+        /// </returns>
+        public static Recorded<Notification<T>> OnCompleted<T>(TimeSpan time)
+        {
+            return Recorded.OnCompleted<T>(time.Ticks);
         }
 
         /// <summary>
@@ -47,6 +60,22 @@ namespace Testeroids.Rx
         }
 
         /// <summary>
+        /// Creates a <see cref="Recorded{Notification}"/> instance with <see cref="NotificationKind.OnError"/> kind.
+        /// </summary>
+        /// <typeparam name="T">Type of the notifications.</typeparam>
+        /// <param name="time">Virtual time the value was produced on.</param>
+        /// <param name="error">The exception to record.</param>
+        /// <returns>
+        /// A new <see cref="Recorded{Notification}"/> instance with <see cref="NotificationKind.OnError"/> kind.
+        /// </returns>
+        public static Recorded<Notification<T>> OnError<T>(
+            TimeSpan time, 
+            Exception error)
+        {
+            return Recorded.OnError<T>(time.Ticks, error);
+        }
+
+        /// <summary>
         /// Creates a <see cref="Recorded{Notification}"/> instance with <see cref="NotificationKind.OnNext"/> kind.
         /// </summary>
         /// <typeparam name="T">Type of the notifications.</typeparam>
@@ -60,6 +89,22 @@ namespace Testeroids.Rx
             T value)
         {
             return new Recorded<Notification<T>>(time, Notification.CreateOnNext(value));
+        }
+
+        /// <summary>
+        /// Creates a <see cref="Recorded{Notification}"/> instance with <see cref="NotificationKind.OnNext"/> kind.
+        /// </summary>
+        /// <typeparam name="T">Type of the notifications.</typeparam>
+        /// <param name="time">Virtual time the value was produced on.</param>
+        /// <param name="value">The value.</param>
+        /// <returns>
+        /// A new <see cref="Recorded{Notification}"/> instance with <see cref="NotificationKind.OnNext"/> kind.
+        /// </returns>
+        public static Recorded<Notification<T>> OnNext<T>(
+            TimeSpan time, 
+            T value)
+        {
+            return Recorded.OnNext(time.Ticks, value);
         }
 
         /// <summary>
@@ -79,6 +124,24 @@ namespace Testeroids.Rx
         {
             // We are using the Equals implementation of Recorded to test which calls Equals on the mocks
             return OnNext(time, value.AsEquatable().Object);
+        }
+
+        /// <summary>
+        /// Creates the a <see cref="Recorded{Notification}"/> with <see cref="NotificationKind.OnNext"/> kind
+        /// converting the passed <see cref="IMock{T}"/> to be <see cref="IEquatable{T}"/>.
+        /// </summary>
+        /// <typeparam name="T">Type of the notifications.</typeparam>
+        /// <param name="time">Virtual time the value was produced on.</param>
+        /// <param name="value">The value.</param>
+        /// <returns>
+        /// A new <see cref="Recorded{Notification}"/> instance with <see cref="NotificationKind.OnNext"/> kind.
+        /// </returns>
+        public static Recorded<Notification<T>> OnNext<T>(
+            TimeSpan time, 
+            IMock<T> value)
+            where T : class
+        {
+            return OnNext(time.Ticks, value);
         }
 
         #endregion
