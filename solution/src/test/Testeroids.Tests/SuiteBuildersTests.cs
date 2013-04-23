1b11b41beddff6b56cdfc0a5a6392832cea8061b
salfab <fabio@seesharp.ch>
2013-04-23 12:46:28 +0200

    replace iterative approcach with recursive one.
     - remove dodgy approach at testing the process. will need to write tests for the recursive algorithm, though

--- solution/src/test/Testeroids.Tests/SuiteBuildersTests.cs
+++ solution/src/test/Testeroids.Tests/SuiteBuildersTests.cs
@@ -7,6 +7,7 @@ namespace Testeroids.Tests
 {
     using System;
     using System.Collections.Generic;
+    using System.Collections.ObjectModel;
     using System.Diagnostics;
     using System.Diagnostics.CodeAnalysis;
     using System.Linq;
@@ -57,7 +58,7 @@ namespace Testeroids.Tests
 
                 private int ReturnedSum { get; set; }
 
-                [TriangulationValues(10, 5)]
+                [TriangulationValues(10)]
                 private int SpecifiedOperand1 { get; set; }
 
                 [TriangulationValues(7, -7, 8)]
@@ -166,90 +167,84 @@ namespace Testeroids.Tests
         public SuiteTestBuilder(Type fixtureType)
             : base(fixtureType)
         {
-            string myString = "";
             this.Parent = new TriangulatedTestMethodFixture(fixtureType);
             foreach (MethodInfo method in fixtureType.GetMethods(BindingFlags.Public | BindingFlags.Instance | BindingFlags.FlattenHierarchy))
             {
-                if (method.Name.StartsWith("then_"))
-                {                    
-                    List<Tuple<PropertyInfo, object>> triangulationValues = null;
+                // Let's ignore methods which are not decorated with the TestAttribute attribute.
+                if (!method.GetCustomAttributes(typeof(TestAttribute), true).Any())
+                {
+                    continue;
+                }
 
-                    // TODO: Read this off the Triangulation attributes.
-                    var triangulatedProperties = method.DeclaringType.FindMembers(MemberTypes.Property,
-                        BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public, 
-                        (info, criteria) => info.IsDefined(typeof(TriangulationValuesAttribute), false),
-                        null).Cast<PropertyInfo>();
+                var triangulatedProperties = method.DeclaringType.FindMembers(
+                    MemberTypes.Property,
+                    BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public,
+                    (info, criteria) => info.IsDefined(typeof(TriangulationValuesAttribute), false),
+                    null).Cast<PropertyInfo>();
 
-                    var possibleValuesForProperties = new Dictionary<PropertyInfo, TriangulatedValuesInformation>();
+                var possibleValuesForProperties = new Dictionary<PropertyInfo, TriangulatedValuesInformation>();
 
-                    foreach (var property in triangulatedProperties)
-                    {
-                        object[] values = property.GetCustomAttributes(typeof(TriangulationValuesAttribute), false).Cast<TriangulationValuesAttribute>().Single().TriangulationValues;                        
-                        var valuesInfo = new TriangulatedValuesInformation(values);
-                        possibleValuesForProperties.Add(property, valuesInfo);
-                    }
+                foreach (var property in triangulatedProperties)
+                {
+                    var values = property.GetCustomAttributes(typeof(TriangulationValuesAttribute), false).Cast<TriangulationValuesAttribute>().Single().TriangulationValues;
+                    var valuesInfo = new TriangulatedValuesInformation(values);
+                    possibleValuesForProperties.Add(property, valuesInfo);
+                }
 
-                    foreach (var triangulatedValuesInformation in this.DiscoverTestMethods(possibleValuesForProperties, method))
-                    {
-                        this.Add(triangulatedValuesInformation);
-                    }                    
+                var propertyInfos = possibleValuesForProperties.Keys.ToArray();
+
+                var triangulationValues = new List<Tuple<PropertyInfo, object>>(propertyInfos.Length);
+                var triangulatedSets = new Collection<IList<Tuple<PropertyInfo, object>>>();
+                this.BuildTriangulationSet(triangulationValues, possibleValuesForProperties, propertyInfos, triangulatedSets);
+
+                foreach (var triangulatedSet in triangulatedSets)
+                {
+                    this.Add(new TriangulatedTestMethod(method, triangulatedSet));
                 }
             }            
         }
 
-        internal IEnumerable<TriangulatedTestMethod> DiscoverTestMethods(Dictionary<PropertyInfo, TriangulatedValuesInformation> possibleValuesForProperties,
-                                         MethodInfo method)
+        /// <summary>
+        /// Recursively builds the triangulation sets and stores them in the specified <paramref name="triangulatedSets"/> accumulator.
+        /// </summary>
+        /// <param name="triangulationValues">The accumulator for the values of the previously visited properties in need of triangulation.</param>
+        /// <param name="possibleValuesForProperties">The different possible values for the properties in need of triangulation. i.e. : the triangulated values.</param>
+        /// <param name="triangulatedProperties">The properties in need of triangulation</param>
+        /// <param name="triangulatedSets">Serves as an accumulator and operation output for the outcome of the recursion.</param>
+        private void BuildTriangulationSet(
+            IList<Tuple<PropertyInfo, object>> triangulationValues,
+            IDictionary<PropertyInfo, TriangulatedValuesInformation> possibleValuesForProperties,
+            IList<PropertyInfo> triangulatedProperties,
+            ICollection<IList<Tuple<PropertyInfo, object>>> triangulatedSets)
         {
-            List<Tuple<PropertyInfo, object>> triangulationValues;
-            bool finishedIterationForCurrentPropertyValues = false;
-            triangulationValues = new List<Tuple<PropertyInfo, object>>();
-            int indexOfLastPropertyIterated = 0;
-
-            while (!finishedIterationForCurrentPropertyValues && triangulationValues.Count <= possibleValuesForProperties.Count)
+            // if the recursion is complete, add the just built set of tiangulated values - representing a complete list of values for all the triangulated properties - to the triangulatedSets.
+            if (triangulationValues.Count == triangulatedProperties.Count)
             {
-                triangulationValues = new List<Tuple<PropertyInfo, object>>();
+                triangulatedSets.Add(triangulationValues);
+                return;
+            }
 
-                var propertyInfos = possibleValuesForProperties.Keys.ToArray();
+            // find the current property:
+            var currentProperty = triangulatedProperties[triangulationValues.Count];
 
-                for (int index = 0; index < propertyInfos.Length; index++)
-                {
-                    var property = propertyInfos[index];
-                    var currentIndex = possibleValuesForProperties[property].CurrentlyProcessedValueIndex;
-                    triangulationValues.Add(new Tuple<PropertyInfo, object>(property, possibleValuesForProperties[property].Values[currentIndex]));
+            var possibleValues = possibleValuesForProperties[currentProperty].Values;
 
-                    var currentIndexOfProperty = index;
+            for (int index = 0; index < possibleValues.Length; index++)
+            {
+                var possibleValue = possibleValues[index];
 
-                    // get the current index ready for next step.
-                    if (currentIndex >= possibleValuesForProperties[property].Values.Length - 1)
-                    {
-                        possibleValuesForProperties[property].CurrentlyProcessedValueIndex = 0;
-                        indexOfLastPropertyIterated = currentIndexOfProperty;
-                        finishedIterationForCurrentPropertyValues = true;
-                    }
-                    else
-                    {
-                        var nextIndexToProcess = currentIndex;
+                // Memory optimization to avoid wasting a list at each recursion:
+                // Create n-1 different lists, with n being the number of possible values for the current property.
+                // Recycle the one which was passed as argument, as the *last* list to be processed (so as to leave the other lists unaffected)
+                var newtriangulationSet = index < possibleValues.Length
+                                               ? triangulationValues.ToList()
+                                               : triangulationValues;
 
-                        var isLastIteratedPropertyTheCurrentOne = indexOfLastPropertyIterated == currentIndexOfProperty;
+                // for a given property, we its possible values (one at each iteration of the current for loop.)
+                newtriangulationSet.Add(new Tuple<PropertyInfo, object>(currentProperty, possibleValue));
 
-                        if (true)
-                        {
-                            var iterationOfPropertyValuesIsNotFinished = isLastIteratedPropertyTheCurrentOne && finishedIterationForCurrentPropertyValues == false;
-
-                            if (!iterationOfPropertyValuesIsNotFinished)
-                            {
-                                nextIndexToProcess++;
-                                indexOfLastPropertyIterated = currentIndexOfProperty;
-                            }
-                        }
-
-                        possibleValuesForProperties[property].CurrentlyProcessedValueIndex = nextIndexToProcess;
-                        finishedIterationForCurrentPropertyValues = false;
-                    }
-                }
-
-                var nUnitTestMethod = new TriangulatedTestMethod(method, triangulationValues);
-                yield return nUnitTestMethod;
+                // ... and we continue the recursion
+                this.BuildTriangulationSet(newtriangulationSet, possibleValuesForProperties, triangulatedProperties, triangulatedSets);
             }
         }
     }
