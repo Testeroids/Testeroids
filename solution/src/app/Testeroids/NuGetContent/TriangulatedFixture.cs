55719462ba8b9b20d0f59aa21c3b5a388ad1a175
salfab <fabio@seesharp.ch>
2013-04-26 13:51:06 +0200

    attempt at just adding files in the nuspec
     - see if conventions and explicit files references can mix.
     - add NuGetContent for "Content" files

---
+++ solution/src/app/Testeroids/NuGetContent/TriangulatedFixture.cs
@@ -0,0 +1,47 @@
+namespace Testeroids.NUnitAddins
+{
+    using System;
+
+    using NUnit.Core.Extensibility;
+
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = false, Inherited = false)]
+    [NUnitAddin(Description = "Testeroid Triangulation Engine")]
+    public class TriangulatedFixture : Attribute, 
+                                       NUnit.Core.Extensibility.IAddin, 
+                                       ISuiteBuilder
+    {
+        #region Public Methods and Operators
+
+        public NUnit.Core.Test BuildFrom(Type type)
+        {
+            return new SuiteTestBuilder(type);
+        }
+
+        public bool CanBuildFrom(Type type)
+        {
+            bool isOk;
+
+            if (type.IsAbstract)
+            {
+                isOk = false;
+            }
+            else
+            {
+                isOk = NUnit.Core.Reflect.HasAttribute(type, "Testeroids.TriangulatedFixture", true);
+            }
+
+            return isOk;
+        }
+
+        public bool Install(IExtensionHost host)
+        {
+            var testCaseBuilders = host.GetExtensionPoint("SuiteBuilders");
+
+            testCaseBuilders.Install(this);
+
+            return true;
+        }
+
+        #endregion
+    }
+}
